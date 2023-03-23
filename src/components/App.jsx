d2ae743e89ff5565fd61b09edc47023c8b727d51
SergSmilya <sergsmilichenko@gmail.com>
2023-03-23 08:34:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,44 @@
 import Feedback from './Feedback/Feedback';
 import React, { Component } from 'react';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export function App() {
+  const [good, setgood] = useState(0);
+  const [neutral, setfneutral] = useState(0);
+  const [bad, setbad] = useState(0);
 
-  onButtonClick = e => {
+  function onButtonClick(e) {
     const key = [e.target.outerText];
     this.setState(prevState => ({ [key]: prevState[key] + 1 }));
-  };
+  }
 
-  countTotalFeedback = () => {
+  function countTotalFeedback() {
     return Object.values(this.state).reduce((total, value) => {
       return (total += value);
     }, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
-
-  render() {
-    return (
-      <div
-        style={{
-          height: '100%',
-          display: 'block',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <Feedback
-          state={this.state}
-          onButtonClick={this.onButtonClick}
-          countTotalFeedback={this.countTotalFeedback}
-          countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-        />
-      </div>
-    );
   }
+
+  function countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'block',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <Feedback
+        state={this.state}
+        onButtonClick={this.onButtonClick}
+        countTotalFeedback={this.countTotalFeedback}
+        countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
+      />
+    </div>
+  );
 }
